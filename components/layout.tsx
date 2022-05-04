91f82080ffe9650471fd3048f6f0fb1e9d1b4566
Franco Beratz <francoberatz.fb@gmail.com>
2022-05-04 15:43:06 -0300

    add proyect

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout: React.FunctionComponent<LayoutProps> = ({ children }) => {
+  return <div className="max-w-6xl mx-auto p-5">{children}</div>
+}
+
+export default Layout
